c6cba6de403a76dc3ff81b7f74651849c12a3833
Gabryel <st_gabriel@hotmail.fr>
2023-08-28 17:22:00 +0200

    Test commit Ambre

--- update.bat
+++ update.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: Mettre à jour le sous-dossier src en fonction de C:\Base\test\
-robocopy "C:\Base\test" "C:\Documentations\src" /MIR
+robocopy "C:\Base\test" "C:\Ambre\src" /MIR
 
 :: Demander à l'utilisateur de saisir le message de commit
 set /p commit_message=Entrez le message de commit : 
 
 :: Se déplacer dans le dossier de travail
-cd C:\Documentations
+cd C:\Ambre
 
 :: Générer le site statique avec mdbook build
 mdbook build
